e9aed01cba27c1141b5ff0fa214d169e8133e19d
Eric Charles <echarles@gmail.com>
2024-07-30 12:20:11 -0700

    fix up __all__ in estimate_pz_task.py

--- python/lsst/meas/pz/estimate_pz_task.py
+++ python/lsst/meas/pz/estimate_pz_task.py
@@ -20,7 +20,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 __all__ = [
-    "EsimatePZTaskBase",
+    "EsimatePZTrainZTask",
     "EstimatePZTrainZConfig",
     "EsimatePZKNNTask",
     "EstimatePZKNNConfig",
