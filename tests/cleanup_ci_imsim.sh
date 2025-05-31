d55f57a8cbf26f3cb6d7040e421e61d19779d767
Dan Taranu <dtaranu@astro.princeton.edu>
2025-05-30 17:40:39 -0700

    More test paredown

--- tests/cleanup_ci_imsim.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-butler remove-collections --no-confirm $CI_IMSIM_DIR/DATA "u/${USER}/pz_rail_testing"
-butler remove-runs --no-confirm $CI_IMSIM_DIR/DATA "u/${USER}/pz_rail_testing*"
-butler remove-runs --no-confirm $CI_IMSIM_DIR/DATA "u/${USER}/pz_models"
