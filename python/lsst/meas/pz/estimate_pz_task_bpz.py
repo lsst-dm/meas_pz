f07855700ec325e6306d5c09dcbd1317767c7ae8
Dan Taranu <dan.s.taranu@gmail.com>
2025-05-30 20:20:18 -0400

    Conditionally import BPZ

--- python/lsst/meas/pz/estimate_pz_task_bpz.py
+++ python/lsst/meas/pz/estimate_pz_task_bpz.py
@@ -19,81 +19,87 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-__all__ = [
-    "EstimatePZBPZAlgoConfig",
-    "EstimatePZBPZAlgoTask",
-    "EstimatePZBPZTask",
-    "EstimatePZBPZConfig",
-]
+try:
+    from rail.estimation.algos.bpz_lite import BPZliteEstimator
+    from rail.estimation.estimator import CatEstimator
+    has_bpz = True
+except ImportError:
+    has_bpz = False
+    __all__ = []
 
-from rail.estimation.algos.bpz_lite import BPZliteEstimator
-from rail.estimation.estimator import CatEstimator
+if has_bpz:
+    __all__ = [
+        "EstimatePZBPZAlgoConfig",
+        "EstimatePZBPZAlgoTask",
+        "EstimatePZBPZTask",
+        "EstimatePZBPZConfig",
+    ]
 
-from .estimate_pz_task import (
-    EstimatePZAlgoConfigBase,
-    EstimatePZAlgoTask,
-    EstimatePZTask,
-    EstimatePZTaskConfig,
-)
-
-
-class EstimatePZBPZAlgoConfig(EstimatePZAlgoConfigBase):
-    """Config for EstimatePZBPZAlgoTask
-
-    This will select and configure the KNearNeighEstimator p(z)
-    estimation algorithm
-
-    """
-
-    @classmethod
-    def estimator_class(cls) -> type[CatEstimator]:
-        return BPZliteEstimator
+    from .estimate_pz_task import (
+        EstimatePZAlgoConfigBase,
+        EstimatePZAlgoTask,
+        EstimatePZTask,
+        EstimatePZTaskConfig,
+    )
 
 
-EstimatePZBPZAlgoConfig._make_fields()
+    class EstimatePZBPZAlgoConfig(EstimatePZAlgoConfigBase):
+        """Config for EstimatePZBPZAlgoTask
+
+        This will select and configure the KNearNeighEstimator p(z)
+        estimation algorithm
+
+        """
+
+        @classmethod
+        def estimator_class(cls) -> type[CatEstimator]:
+            return BPZliteEstimator
 
 
-class EstimatePZBPZAlgoTask(EstimatePZAlgoTask):
-    """SubTask that runs RAIL BPZ algorithm for p(z) estimation
-
-    See https://github.com/LSSTDESC/rail_bpz/blob/main/src/rail/estimation/algos/bpz_lite.py  # noqa
-    for algorithm implementation.
-
-    """
-
-    ConfigClass = EstimatePZBPZAlgoConfig
-    _DefaultName = "estimatePZBPZAlgo"
+    EstimatePZBPZAlgoConfig._make_fields()
 
 
-class EstimatePZBPZConfig(EstimatePZTaskConfig):
-    """Config for EstimatePZBPZTask
+    class EstimatePZBPZAlgoTask(EstimatePZAlgoTask):
+        """SubTask that runs RAIL BPZ algorithm for p(z) estimation
 
-    Overrides setDefaults to use BPZ algorithm
-    """
+        See https://github.com/LSSTDESC/rail_bpz/blob/main/src/rail/estimation/algos/bpz_lite.py  # noqa
+        for algorithm implementation.
 
-    def setDefaults(self) -> None:
-        self.pz_algo.retarget(EstimatePZBPZAlgoTask)
-        self.pz_algo.stage_name = "bpz"
-        self.pz_algo.output_mode = "return"
-        self.pz_algo.bands_to_convert = ["u", "g", "r", "i", "z", "y"]
-        self.pz_algo.ref_band = self.pz_algo.mag_template.format(band='i')
-        self.pz_algo.bands = self.pz_algo.get_mag_name_list()
-        self.pz_algo.err_bands = self.pz_algo.get_mag_err_name_list()
-        self.pz_algo.mag_limits = self.pz_algo.get_mag_lim_dict()
-        self.pz_algo.filter_list = [
-            "DC2LSST_u",
-            "DC2LSST_g",
-            "DC2LSST_r",
-            "DC2LSST_i",
-            "DC2LSST_z",
-            "DC2LSST_y",
-        ]
-        self.pz_algo.zp_errors = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
-        self.pz_algo.band_a_env = self.pz_algo.get_band_a_env_dict()
+        """
+
+        ConfigClass = EstimatePZBPZAlgoConfig
+        _DefaultName = "estimatePZBPZAlgo"
 
 
-class EstimatePZBPZTask(EstimatePZTask):
-    """Task that runs RAIL BPZ algorithm for p(z) estimation"""
+    class EstimatePZBPZConfig(EstimatePZTaskConfig):
+        """Config for EstimatePZBPZTask
 
-    ConfigClass = EstimatePZBPZConfig
-    _DefaultName = "estimatePZBPZ"
+        Overrides setDefaults to use BPZ algorithm
+        """
+
+        def setDefaults(self) -> None:
+            self.pz_algo.retarget(EstimatePZBPZAlgoTask)
+            self.pz_algo.stage_name = "bpz"
+            self.pz_algo.output_mode = "return"
+            self.pz_algo.bands_to_convert = ["u", "g", "r", "i", "z", "y"]
+            self.pz_algo.ref_band = self.pz_algo.mag_template.format(band='i')
+            self.pz_algo.bands = self.pz_algo.get_mag_name_list()
+            self.pz_algo.err_bands = self.pz_algo.get_mag_err_name_list()
+            self.pz_algo.mag_limits = self.pz_algo.get_mag_lim_dict()
+            self.pz_algo.filter_list = [
+                "DC2LSST_u",
+                "DC2LSST_g",
+                "DC2LSST_r",
+                "DC2LSST_i",
+                "DC2LSST_z",
+                "DC2LSST_y",
+            ]
+            self.pz_algo.zp_errors = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
+            self.pz_algo.band_a_env = self.pz_algo.get_band_a_env_dict()
+
+
+    class EstimatePZBPZTask(EstimatePZTask):
+        """Task that runs RAIL BPZ algorithm for p(z) estimation"""
+
+        ConfigClass = EstimatePZBPZConfig
+        _DefaultName = "estimatePZBPZ"
