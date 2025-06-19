2edf7b129379c54bc6597d4551a182906ad22b2d
Eric Charles <echarles@gmail.com>
2025-06-18 18:29:19 -0700

    added id_col and calc_summary_stats to default configurations

--- python/lsst/meas/pz/estimate_pz_task_knn.py
+++ python/lsst/meas/pz/estimate_pz_task_knn.py
@@ -75,6 +75,8 @@ class EstimatePZKNNConfig(EstimatePZTaskConfig):
         self.pz_algo.bands = self.pz_algo.get_mag_name_list()
         self.pz_algo.mag_limits = self.pz_algo.get_mag_lim_dict()
         self.pz_algo.band_a_env = self.pz_algo.get_band_a_env_dict()
+        self.pz_algo.id_col = "objectId"
+        self.pz_algo.calc_summary_stats = True
 
 
 class EstimatePZKNNTask(EstimatePZTask):
