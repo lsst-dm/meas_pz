0aef4c5e527ba708f5f08330b5a0c2907d018201
Eric Charles <echarles@gmail.com>
2025-06-18 17:18:30 -0700

    extract numpy array from astropy.MaskedArray to avoid issues with some of the RAIL algorithms

--- python/lsst/meas/pz/estimate_pz_task.py
+++ python/lsst/meas/pz/estimate_pz_task.py
@@ -428,8 +428,8 @@ class EstimatePZAlgoTask(Task, ABC):
         mag_dict = {}
         # loop over bands, make mags and mag errors and fill dict
         for band in flux_names.keys():
-            fluxVals = fluxes[flux_names[band]]
-            fluxErrVals = fluxes[flux_err_names[band]]
+            fluxVals = fluxes[flux_names[band]].data
+            fluxErrVals = fluxes[flux_err_names[band]].data
             mag_dict[mag_names[band]] = self._flux_to_mag(
                 fluxVals,
                 mag_offset,
